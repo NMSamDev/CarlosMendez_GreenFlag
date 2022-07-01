1a12fc5792ca2d6b534e551fdd45c4dab67fb69c
NMSamDev <c_s_m_h_13@hotmail.com>
2022-07-01 11:13:37 -0400

    first commit

---
+++ app/src/main/java/com/example/carlosmendez_greenflag/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.carlosmendez_greenflag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
